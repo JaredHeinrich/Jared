a051bc146762263120763c47e93fc9bd6ebb3d49
Jared Heinrich <jared.heinrich@sap.com>
2024-11-17 11:18:39 +0100

    lazy setup

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,4 @@
 return {
-    -- {'neovim/nvim-lspconfig'},
-    -- {'williamboman/mason.nvim'},
-    -- {'williamboman/mason-lspconfig.nvim'},
-    -- {'hrsh7th/nvim-cmp'},
-    -- {'hrsh7th/cmp-buffer'},
-    -- {'hrsh7th/cmp-path'},
-    -- {'hrsh7th/cmp-nvmi-lsp'},
     {
         'folke/lazydev.nvim',
         ft = 'lua',
--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,21 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+    config = function ()
+        local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = {},
-          sync_install = false,
-          auto_install = true,
-          highlight = {
-              enable = true,
-              disable = { "latex" },
-              additional_vim_regex_highlighting = false,
-          },
-          indent = { enable = true },  
+        configs.setup({
+            ignore_install = {},
+            modules = {},
+            ensure_installed = {'lua'},
+            sync_install = false,
+            auto_install = false,
+            highlight = {
+                enable = true,
+                disable = { "latex" },
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
         })
     end
 }
