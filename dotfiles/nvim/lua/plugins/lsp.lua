e5b47287afae79f863cec9ad5a97d9602923bbe6
Jared Heinrich <jared.heinrich@sap.com>
2025-07-04 07:08:28 +0200

    typst openPdf

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -160,6 +160,29 @@ return {
                 settings = {
                     exportPdf = "onSave",
                 },
+                on_attach = function(client, bufnr)
+                    vim.keymap.set("n", "<leader>tp", function()
+                        client:exec_cmd({
+                            title = "pin",
+                            command = "tinymist.pinMain",
+                            arguments = { vim.api.nvim_buf_get_name(0) },
+                        }, { bufnr = bufnr })
+                    end, { desc = "[T]inymist [P]in", noremap = true })
+                    vim.keymap.set("n", "<leader>tu", function()
+                        client:exec_cmd({
+                            title = "unpin",
+                            command = "tinymist.pinMain",
+                            arguments = { vim.v.null },
+                        }, { bufnr = bufnr })
+                    end, { desc = "[T]inymist [U]npin", noremap = true })
+                    vim.api.nvim_create_user_command("OpenPdf", function()
+                        local filepath = vim.api.nvim_buf_get_name(0)
+                        if filepath:match("%.typ$") then
+                            local pdf_path = filepath:gsub("%.typ$", ".pdf")
+                            vim.system({ "zathura", pdf_path })
+                        end
+                    end, {})
+                end,
             })
 
 
